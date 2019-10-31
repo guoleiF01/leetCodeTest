d23e80eb3168d2a1e3fb2dda933d05d98bda35ff
guoleiF01 <597358068@qq.com>
2019-10-31 14:23:10 +0800

    Initial commit

---
+++ src/TwoSum_1.java
@@ -0,0 +1,47 @@
+/**
+ *
+ * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+ You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+ Example:
+
+ Given nums = [2, 7, 11, 15], target = 9,
+
+ Because nums[0] + nums[1] = 2 + 7 = 9,
+ return [0, 1].
+ -----------------------------------------------------------------------------------------
+ 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+
+ 你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
+
+ 示例:
+
+ 给定 nums = [2, 7, 11, 15], target = 9
+
+ 因为 nums[0] + nums[1] = 2 + 7 = 9
+ 所以返回 [0, 1]
+
+ *
+ *
+ */
+public class TwoSum_1 {
+    public int[] twoSum(int[] nums, int target) {
+
+        for(int i=0;i<nums.length;i++)
+        {
+            for (int j=0;i<nums.length;j++)
+            {
+                if(nums[i]+nums[j]==target)
+                {
+                    return new int[]{i,j};
+                }
+            }
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
